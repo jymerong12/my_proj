ae80e4ce7b97ed44e63014a978596330d24f8044
JIYEON <jymerong12@naver.com>
2021-01-20 15:15:42 +0900

    my project 첫번째 커밋

---
+++ src/org/comstudy21/HelloWorldMyHome.java
@@ -0,0 +1,9 @@
+package org.comstudy21;
+
+public class HelloWorldMyHome {
+	public static void main(String[] args) {
+		String user = "Hong-gildong";
+		
+		System.out.println("User : " + user);
+	}
+}
